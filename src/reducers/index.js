d7bcd52ea2bb066d98722607fa03b17203e91726
Bede <bede.overend@gmail.com>
2016-07-07 13:51:40 +1000

    combine all reducers into reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,12 @@
-export default (state, action) => {
-  return state;
-}
+import { combineReducers } from 'redux';
+import authentication from './authentication';
+import options from './options';
+import imports from './imports';
+
+const reducer = combineReducers({
+  authentication,
+  options,
+  imports
+});
+
+export default reducer;
