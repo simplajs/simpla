97576ef7f3df6d6e82f346e6c661312c619f682a
Bede Overend <bede.overend@gmail.com>
2017-01-27 11:08:29 +1100

    use beta API as  default server

--- src/constants/options.js
+++ src/constants/options.js
@@ -1,3 +1,3 @@
-export const AUTH_SERVER = 'https://api.simpla.io';
+export const AUTH_SERVER = 'https://api-beta.xb177olenlgv.netdna-cdn.com';
 export const ELEMENTS_SERVER = 'https://elements.simpla.io';
 export const APP_SERVER = 'https://app.simpla.io';
