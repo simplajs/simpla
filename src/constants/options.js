3b429d0ee1645d026e90f622470a14e37ba520be
Bede Overend <bede.overend@gmail.com>
2017-01-27 15:39:14 +1100

    fix incorrect beta API server url

--- src/constants/options.js
+++ src/constants/options.js
@@ -1,3 +1,3 @@
-export const AUTH_SERVER = 'https://api-beta.xb177olenlgv.netdna-cdn.com';
+export const AUTH_SERVER = 'https://api-beta-xb177olenlgv.netdna-ssl.com';
 export const APP_SERVER = 'https://app.simpla.io';
 export const WC_POLYFILL = 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.23/webcomponents-lite.min.js';
