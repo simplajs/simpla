7d659f17a3f024a18abdd06afc28d3556075fb84
Bede Overend <bede.overend@gmail.com>
2017-02-24 13:14:32 +1100

    import simpla.js into tests first - ensures polyfills are loaded

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
+// ./simpla should be imported first as it imports the polyfills
+import './simpla';
 import './actions';
 import './plugins';
 import './reducers';
 import './utils';
-import './simpla';
